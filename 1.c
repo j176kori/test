e8d2b4b35057b98d9bc7340cb15dbeaece009f93
j176kori <j520176b@mails.cc.ehime-u.ac.jp>
2024-06-25 16:34:57 +0900

    Update 1.c

--- 1.c
+++ 1.c
@@ -7,7 +7,7 @@ int main(void)
   printf("n = ");
   scanf("%d", &n);
 
-  if( (n % 1) == 0 ){
+  if( (n & 1) == 0 ){ //1 == 0よりn & 1が先に実行されるように変更した。
     printf("%d は偶数です\n", n);
   }else{
     printf("%d は奇数です\n", n);
