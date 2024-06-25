005e6dcef271ad54abddcc71a86fad3f60f59719
j176kori <j520176b@mails.cc.ehime-u.ac.jp>
2024-06-25 16:30:01 +0900

    Create a.php

---
+++ a.php
@@ -0,0 +1,10 @@
+#!/usr/bin/php
+<?php
+  foreach(file('/usr/share/dict/words') as $line){
+    // strpos() 関数を使って、文字列 "xy" が含まれる単語を echo "$line"; で\\
+表示しなさい。
+     if (strpos($line, 'xy') !== false) {
+        echo "$line";
+    }
+  }
+?>
